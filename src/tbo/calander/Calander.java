5a354f98ff2e2f5faef387a616d78cd97671b52a
jungsekwun <jungsekwun@192.168.0.5>
2021-09-28 19:03:19 +0900

    unpdate using class method

--- src/tbo/calander/Calander.java
+++ src/tbo/calander/Calander.java
@@ -4,21 +4,32 @@ import java.util.Scanner;
 
 public class Calander {
 	
-	public static void main(String[] args) {
+	private static int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public static int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+	
+	public static void printSampleCalander() {
 		System.out.println(" 일 월  화 수  목 금  토 ");
 		System.out.println("---------------------");
 		System.out.println("  1  2  3  4  5  6  7");
 		System.out.println("  8  9 10 11 12 13 14");
 		System.out.println(" 15 16 17 18 19 20 21");
-		System.out.println(" 22 23 24 25 26 27 28");
-		
-		System.out.println("달을 입력하세요.");
+		System.out.println(" 22 23 24 25 26 27 28");	
+	}
+	
+	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
+		Calander cal = new Calander();
+		
+		cal.printSampleCalander();
+		System.out.println("달을 입력하세요.");
+		
 		int month = scanner.nextInt();
 		
-		int[] maxMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		System.out.printf("%d월은 %d일까지 있습니다.", month, maxMonth[month - 1]);
+		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
 		scanner.close();
 	}
 
